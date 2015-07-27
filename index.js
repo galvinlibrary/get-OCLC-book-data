858971f7217cea77730a0404d0ccab6e5977915c
Christine McClure <camcclure@gmail.com>
2015-07-27 11:21:16 -0500

    changed output header line

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function init(callback){
         if (error) throw error;
       });
     }
-    fs.appendFile(path+dataFile,  'isbn\,title\,author\,edition\r\n', function (error) { 
+    fs.appendFile(path+dataFile,  '"isbn","title","author","edition"\r\n', function (error) { 
       if (error) throw error;
     });
   });
