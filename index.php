05c9b4cd393849c6abf40bafa932f9c1e2b2fd7b
Christine McClure <camcclure@gmail.com>
2015-10-19 16:51:01 -0500

    remove node files

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
 
-$key=getenv('OCLC_DEV_KEY');
-$url="http://www.worldcat.org/webservices/catalog/content/isbn/978-0-02-391341-9?wskey=" . $key;
+$wskey=getenv('OCLC_DEV_KEY');
+$url="http://www.worldcat.org/webservices/catalog/content/isbn/978-0-02-391341-9?wskey=" . $wskey;
 
-echo "<p>$url</p>";
-$file = file_get_contents($url);
-echo "<p>$file</p>";
-
+if (($response_xml_data = file_get_contents($url))===false){
+    echo "Error fetching XML\n";
+} 
+else {
+   libxml_use_internal_errors(true);
+   $data = simplexml_load_string($response_xml_data);
+   if (!$data) {
+       echo "Error loading XML\n";
+       foreach(libxml_get_errors() as $error) {
+           echo "\t", $error->message;
+       }
+   } else {
+      print_r($data);
+   }
+}
+?>
+ 
 ?>
