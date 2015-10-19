482a4cd5e9b1f4d2388c842aa4bf6001e388f2ba
Christine McClure <camcclure@gmail.com>
2015-10-19 17:37:47 -0500

    switch example working in function

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
+function get_record_info($record, $type){
+  switch ($type){
+    case title:
+      foreach($record->datafield as $item){
+        if ($item[@tag]==245){
+          foreach ($item->subfield as $subField){
+            if ($subField[@code]=="a"){
+              $pattern='/\s\/$/'; 
+              $string=$subField;
+              $replacement="";
+              break;
+            }
+          }
+        }
+      }      
+      
+  }
+  return preg_replace($pattern, $replacement, $string);
+}
+
 $wskey=getenv('OCLC_DEV_KEY');
 $url="http://www.worldcat.org/webservices/catalog/content/isbn/978-0-02-391341-9?wskey=" . $wskey;
 
@@ -8,18 +28,16 @@ if (($response_xml_data = file_get_contents($url))===false){
 } 
 else {
    libxml_use_internal_errors(true);
-   $data = simplexml_load_string($response_xml_data);
-   if (!$data) {
+   $dataObj = simplexml_load_string($response_xml_data);
+   if (!$dataObj) {
        echo "Error loading XML\n";
        foreach(libxml_get_errors() as $error) {
            echo "\t", $error->message;
        }
    } else {
-      print_r($data);
-      foreach($data->datafield as $item){
-      echo "<p>***</p>";
-      echo $item[@tag];
-      }
+      print_r($dataObj);
+      $title=get_record_info($dataObj, "title");
+      echo "<p>$title</p>";
    }
 }
  
