c869ab30119e76be9cb1e3a20f307f3a55bfb591
Christine McClure <camcclure@gmail.com>
2015-10-26 14:12:24 -0500

    move function to file

--- functions.php
+++ functions.php
@@ -40,6 +40,41 @@ function get_list_of_input_files(){
   return($inputFiles);
 }
 
+function get_oclc_worldcat_record($isbn){
+  global $wskey;
+  
+  if (!$isbn){
+    log_message("isbn is blank");
+    return -1;
+  }
+  if (!$wskey){
+    log_message("cannot get system variable OCLC_DEV_KEY");
+    die;
+  }
+  $url="http://www.worldcat.org/webservices/catalog/content/isbn/" . $isbn . "?wskey=" . $wskey;
+  if (($response_xml_data = file_get_contents($url))===false){
+      echo "Error fetching XML\n";
+  } 
+  else {
+     libxml_use_internal_errors(true);
+     $dataObj = simplexml_load_string($response_xml_data);
+     if (!$dataObj) {
+         echo "Error loading XML\n";
+         foreach(libxml_get_errors() as $error) {
+             echo "\t", $error->message;
+         }
+     } else {
+        //print_r($dataObj);
+        $title=get_record_info($dataObj, "title");
+        $author=get_record_info($dataObj, "author");
+        $edition=get_record_info($dataObj, "edition");
+        echo "<p>TITLE = $title and AUTHOR = $author and EDITION = $edition</p>";
+     }
+  }
+  
+}
+
+
 function get_record_info($record, $type){
   $localDebug=false;
   $regExMatch="";
--- index.php
+++ index.php
@@ -4,43 +4,7 @@ $wskey=getenv('OCLC_DEV_KEY');
 $logFile=date("Y-m-d").".log";
 include_once 'functions.php';
 
-function get_oclc_worldcat_record($isbn){
-  global $wskey;
-  
-  if (!$isbn){
-    log_message("isbn is blank");
-    return -1;
-  }
-  if (!$wskey){
-    log_message("cannot get system variable OCLC_DEV_KEY");
-    die;
-  }
-  $url="http://www.worldcat.org/webservices/catalog/content/isbn/" . $isbn . "?wskey=" . $wskey;
-  if (($response_xml_data = file_get_contents($url))===false){
-      echo "Error fetching XML\n";
-  } 
-  else {
-     libxml_use_internal_errors(true);
-     $dataObj = simplexml_load_string($response_xml_data);
-     if (!$dataObj) {
-         echo "Error loading XML\n";
-         foreach(libxml_get_errors() as $error) {
-             echo "\t", $error->message;
-         }
-     } else {
-        //print_r($dataObj);
-        $title=get_record_info($dataObj, "title");
-        $author=get_record_info($dataObj, "author");
-        $edition=get_record_info($dataObj, "edition");
-        echo "<p>TITLE = $title and AUTHOR = $author and EDITION = $edition</p>";
-     }
-  }
-  
-}
-
-
 create_log_file();
-
 $filesArr=get_list_of_input_files();
 print_r($filesArr);
 
