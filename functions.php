e0dbfcc1deacf4abe6d28c9b75f40964ea4b1249
Christine McClure <camcclure@gmail.com>
2015-10-26 10:45:44 -0500

    put functions in separate file

---
+++ functions.php
@@ -0,0 +1,82 @@
+<?php
+
+function get_record_info($record, $type){
+  $localDebug=false;
+  $regExMatch="";
+  $recordArr=array(
+    "title"=>"245", 
+    "author"=>"245", 
+    "edition"=>"250"
+  );
+  if (!array_key_exists($type,$recordArr))return "error";
+  
+  $marcField = $recordArr[$type];
+  
+  if ($localDebug) echo "<p>Looking for $type in $marcField</p>";
+  
+  foreach($record->datafield as $item){
+    if ($item[@tag]==$marcField){
+      
+      switch ($type){
+
+        case "title":
+          $eleStr=loop_record_to_find_code($item, "a");
+          if ($eleStr){
+              $regExMatch="/ \/$/";
+              $regExRepl="";
+              break;
+          }
+
+        break;
+
+        case "author":
+          $eleStr=loop_record_to_find_code($item, "c");
+            if ($eleStr){
+              $regExMatch="/.$/";
+              $regExRepl="";
+              break;
+            }  
+        break;
+
+        case "edition":
+          $eleStr=loop_record_to_find_code($item, "a");
+            if ($eleStr){
+              break;
+            }          
+        break;
+        
+        default:
+          return -1;
+
+      }//end switch
+      if ($regExMatch)
+        return preg_replace($regExMatch,$regExRepl,$eleStr);
+      else
+        return $eleStr;
+    } // end if
+    else{
+      continue;
+    }
+  }//end foreach      
+
+}// end get_record_info function
+
+
+function log_message ($msg){
+  global $logFile;
+  $fh = fopen($logFile, 'a') or die("can't open file");
+  $msg .= "\n";
+  fwrite($fh, $msg);
+  fclose($fh);  
+}
+
+function loop_record_to_find_code($item, $check){
+  for($i=0; $i<count($item->subfield); $i++){
+    if ($item->subfield[$i][@code]==$check)
+      return $item->subfield[$i];
+    else
+      continue;
+  }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,76 +1,10 @@
 <?php
 date_default_timezone_set('America/Chicago');
 $logFile=date("Y-m-d").".log";
+include_once 'functions.php';
 
-function loop_record_to_find_code($item, $check){
-  for($i=0; $i<count($item->subfield); $i++){
-    if ($item->subfield[$i][@code]==$check)
-      return $item->subfield[$i];
-    else
-      continue;
-  }
-}
 
-function get_record_info($record, $type){
-  $localDebug=false;
-  $regExMatch="";
-  $recordArr=array(
-    "title"=>"245", 
-    "author"=>"245", 
-    "edition"=>"250"
-  );
-  if (!array_key_exists($type,$recordArr))return "error";
-  
-  $marcField = $recordArr[$type];
-  
-  if ($localDebug) echo "<p>Looking for $type in $marcField</p>";
-  
-  foreach($record->datafield as $item){
-    if ($item[@tag]==$marcField){
-      
-      switch ($type){
 
-        case "title":
-          $eleStr=loop_record_to_find_code($item, "a");
-          if ($eleStr){
-              $regExMatch="/ \/$/";
-              $regExRepl="";
-              break;
-          }
-
-        break;
-
-        case "author":
-          $eleStr=loop_record_to_find_code($item, "c");
-            if ($eleStr){
-              $regExMatch="/.$/";
-              $regExRepl="";
-              break;
-            }  
-        break;
-
-        case "edition":
-          $eleStr=loop_record_to_find_code($item, "a");
-            if ($eleStr){
-              break;
-            }          
-        break;
-        
-        default:
-          return -1;
-
-      }//end switch
-      if ($regExMatch)
-        return preg_replace($regExMatch,$regExRepl,$eleStr);
-      else
-        return $eleStr;
-    } // end if
-    else{
-      continue;
-    }
-  }//end foreach      
-
-}// end get_record_info function
 
 $wskey=getenv('OCLC_DEV_KEY');
 $url="http://www.worldcat.org/webservices/catalog/content/isbn/978-0-02-391341-9?wskey=" . $wskey;
@@ -96,13 +30,5 @@ else {
    }
 }
 
-
-function log_message ($msg){
-  global $logFile;
-  $fh = fopen($logFile, 'a') or die("can't open file");
-  $msg .= "\n";
-  fwrite($fh, $msg);
-  fclose($fh);  
-}
 
 ?>
